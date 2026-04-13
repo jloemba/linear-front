cb03196f7223a680c150be5a6945ebe8d1e49b64
Joris Loemba <jorisloemba@gmail.com>
2026-04-13 21:32:32 +0200

    fix(build): fix build

--- src/hooks/useClothEditorHook/useClothEditor.ts
+++ src/hooks/useClothEditorHook/useClothEditor.ts
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, type FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   createCloth,
